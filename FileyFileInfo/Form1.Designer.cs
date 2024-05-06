fae6fc9e2acf41855225916ef997a8220a5de2ad
Abraham V. R <sneak_89@hotmail.com>
2024-05-06 09:43:08 -0600

    Agregar archivos de proyecto.

---
+++ FileyFileInfo/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FileyFileInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(59, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Crear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(223, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(420, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Escribir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(591, 69);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 45);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Leer";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 145);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(680, 159);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(123, 335);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(145, 49);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Copiar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(336, 335);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(194, 49);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Propiedades";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ FileyFileInfo/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace FileyFileInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Si el archivo no existe entonces crealo
+            if(!File.Exists("ArchivoDos.txt"))
+            {
+                //File.Create("ArchivoUno.txt");
+                FileInfo archivo = new FileInfo("ArchivoDos.txt");
+                archivo.Create(); 
+            }
+
+             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.Delete("ArchivoDos.txt"); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("ArchivoTres.txt", FileMode.OpenOrCreate,FileAccess.Write );
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("Esta es la primera linea");
+            sw.WriteLine("Aqui acabo de escribir otra cosa");
+            sw.WriteLine("Aqui escribi otra cosa"); 
+            sw.Flush(); //Para mandar lo escrito
+            sw.Close();
+            fs.Close(); 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("ArchivoTres.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string texto = "";
+            //Ciclo while nos permite leer hasta cierto condicion
+            //La condicion será hasta que ya no existan caracteres
+            while (!sr.EndOfStream)
+            {
+                texto += sr.ReadLine() + Environment.NewLine;
+            }
+            sr.Close();
+            fs.Close(); 
+            textBox1.Text = texto;
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            File.Copy("ArchivoTres.txt", @"C:\Temporal\ArchivoTres.txt"); 
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FileInfo miArchivo = new FileInfo("ArchivoTres.txt");
+            string propiedades = "";
+            propiedades += miArchivo.Name + Environment.NewLine;
+            propiedades += miArchivo.FullName + Environment.NewLine;
+            propiedades += miArchivo.LastAccessTime + Environment.NewLine;
+            propiedades += miArchivo.Length + Environment.NewLine;
+            propiedades += miArchivo.LastWriteTimeUtc + Environment.NewLine;
+            MessageBox.Show(propiedades); 
+        }
+    }
+}
